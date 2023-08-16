ea87cfcaffa6d5a6c89e23dd810291de35772876
Mohammed Hardi <41858062+mhabdulbaaki@users.noreply.github.com>
2023-08-16 22:23:09 +0200

    removed old code

--- template-code-python/order_management/__init__.py
+++
@@ -1,7 +0,0 @@
-from .stock import Stock, Product
-from .cart import Cart
-from .wrapper import Wrapper
-from .prescription import Prescription
-from .books import BookRecords, Sale
-from .user_management import UserManagement, User
-from .menu import Menu
